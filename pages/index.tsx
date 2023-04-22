893e6c1480899eae0a5e1f66713a178d2dff5c4e
nathanhleung <nathanhleung@users.noreply.github.com>
2023-04-21 23:13:32 -0700

    feat: use new subdomain on main page

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,7 +10,7 @@ export default function Home() {
     >
 
       <div className="relative flex flex-col place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <h1 className="text-5xl font-bold mb-10">Dynamic Images</h1>
+        <h1 className="text-5xl font-bold mb-10">img.natecation.xyz</h1>
         <h2 className="text-xl opacity-50">A small API to generate dynamic images (email signatures, Github profile README images, etc.)</h2>
       </div>
 
